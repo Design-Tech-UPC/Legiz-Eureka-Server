ed7376bc33f79989bba5485b288d913b836129e7
Hans Retes <u20171c266@upc.edu.pe>
2022-11-01 22:59:44 -0500

    Added Eureka Config Server

---
+++ src/main/java/com/designtech/eurekaserver/LegizEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.designtech.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class LegizEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegizEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/designtech/eurekaserver/LegizEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designtech.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegizEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
